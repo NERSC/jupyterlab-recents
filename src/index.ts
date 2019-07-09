ddb12175f0a6e9276aeadda24a5779dc91de75bf
Trevor Slaton <trevor.slaton@gmail.com>
2019-07-09 16:36:11 -0700

    Stub code

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,48 @@ import {
   IMainMenu
 } from '@jupyterlab/mainmenu';
 
-/**
- * Initialization data for the jupyterlab-recents extension.
- */
+namespace PluginIDs {
+  export const recents = 'jupyterlab-recents';
+}
+
+namespace StateIDs {
+  export const recents = `${PluginIDs}:recents`;
+}
+
+namespace types {
+  export type Recent = {
+      root: string;
+      path: string;
+      contentType: string;
+  }
+}
+
+class RecentsManager {
+  private stateDB: IStateDB;
+
+  constructor(stateDB: IStateDB) {
+    this.stateDB = stateDB;
+  }
+
+  async loadRecents() {
+    const recents = await this.stateDB.fetch(StateIDs.recents);
+    return recents;
+  }
+
+  saveRecents(recents: Array<types.Recent>) {
+    this.stateDB.save(StateIDs.recents, recents);
+  }
+}
+
+
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-recents',
+  id: PluginIDs.recents,
   autoStart: true,
   requires: [IStateDB, IMainMenu],
   activate: (app: JupyterFrontEnd, stateDB: IStateDB, mainMenu: IMainMenu) => {
     console.log('JupyterLab extension jupyterlab-recents is activated!');
+    const recentsManager = new RecentsManager(stateDB);
+    console.log(recentsManager);
   }
 };
 
